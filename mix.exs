2f76f1a9fe5c80bf31d63c3359bd0a0703516f65
lorenzosinisi <lasslo.net@gmail.com>
2019-09-09 17:17:34 +0200

    Add package info

--- mix.exs
+++ mix.exs
@@ -1,29 +1,45 @@
 defmodule Exkontist.MixProject do
   use Mix.Project
-
+  @version "0.1.1"
   def project do
     [
       app: :exkontist,
-      version: "0.1.0",
-      elixir: "~> 1.8",
-      start_permanent: Mix.env() == :prod,
+      version: @version,
+      elixir: "~> 1.9",
+      docs: [extras: ["README.md"], main: "readme", source_ref: "v#{@version}"],
+      source_url: "https://github.com/lorenzosinisi/exkontist",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger],
-      mod: {Exkontist.Application, []}
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:httpoison, "~> 1.5"},
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:ex_doc, "~> 0.16", only: :dev},
+      {:earmark, "~> 1.2", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    (not official) Konstist client in Elixir
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Lorenzo Sinisi"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/lorenzosinisi/exkontist"}
     ]
   end
 end
