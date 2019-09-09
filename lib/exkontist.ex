f672bdeb67455d385ac0803346fb3773b12bcb7c
lorenzosinisi <lasslo.net@gmail.com>
2019-09-09 15:59:13 +0200

    Bootstrap project

---
+++ lib/exkontist.ex
@@ -0,0 +1,18 @@
+defmodule Exkontist do
+  @moduledoc """
+  Documentation for Exkontist.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exkontist.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exkontist_test.exs
@@ -0,0 +1,8 @@
+defmodule ExkontistTest do
+  use ExUnit.Case
+  doctest Exkontist
+
+  test "greets the world" do
+    assert Exkontist.hello() == :world
+  end
+end
